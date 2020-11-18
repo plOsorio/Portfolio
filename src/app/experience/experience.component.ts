b883588543ddb07454208d7f606589723337fea5
Pedro Osorio <pl.osoriorodriguez@gmail.com>
2020-11-18 10:21:23 +0100

    port v 0.2 before fixing the web maquetation

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioServicesService } from '../services/portfolio-services.service';
 
 @Component({
   selector: 'app-experience',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+//We create the constructor linked with service folder then with the bd firebase
+ //1first of all we have to create a constructor, 2 we import the service folder, 3 we have to worke with the filing json since the webservices 
+  constructor(public portfolio: PortfolioServicesService) { }
 
   ngOnInit(): void {
   }
--- src/app/services/portfolio-services.service.ts
+++ src/app/services/portfolio-services.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,39 @@ import { Injectable } from '@angular/core';
 })
 export class PortfolioServicesService {
 
-  constructor() { }
+  Skills: any[] = [];
+  Education: any[] = [];
+  Experience: any[] = [];
+  //activamos el http client importandolo
+  constructor(private http: HttpClient) { 
+    console.log("Json Firabase");
+    this.LoadSkills();
+    this.LoadEducation();
+    this.LoadExperience();
+  }
+
+  private LoadSkills(){
+    this.http.get('https://portfolio-3867f.firebaseio.com/Skills.json')
+    .subscribe((resp: any[]) =>{
+      this.Skills = resp;
+      console.log(resp);
+    });
+  }
+  private LoadEducation(){
+    this.http.get('https://portfolio-3867f.firebaseio.com/Education.json')
+    .subscribe((resp: any[]) =>{
+      this.Education= resp;
+      console.log(resp);
+    });
+  }
+  private LoadExperience(){
+    this.http.get('https://portfolio-3867f.firebaseio.com/Experience.json')
+    .subscribe((resp: any[]) =>{
+      this.Experience= resp;
+      console.log(resp);
+    });
+  }
 }
+
+//1To import a Json file from bd, we have to create a library in app.module.ts in array imports next browserModule
+//2  next browserModdule we insert HttpClientModule
